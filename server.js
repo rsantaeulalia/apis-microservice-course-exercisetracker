0c4ea5960dfa9289753c9756e71aa2511b7fae7d
rsantaeulalia <rodrigo.santaeulalia@gmail.com>
2021-06-22 20:26:51 -0300

    added users endpoint

--- server.js
+++ server.js
@@ -1,7 +1,30 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+var bodyParser = require('body-parser');
+
 require('dotenv').config()
+
+var mongo = require('mongodb');
+var mongoose = require('mongoose');
+
+// Basic Configuration
+const port = process.env.PORT || 3000;
+
+mongoose.connect(process.env.DB_URI, { useNewUrlParser: true });
+
+const exerciseSchema = new mongoose.Schema({
+  description: { type: String, required: true },
+  duration: { type: Number, required: true },
+  date: Date
+});
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  exercises: [exerciseSchema]
+});
+
+const User = mongoose.model('ShortUrl', userSchema);
 
 app.use(cors())
 app.use(express.static('public'))
@@ -10,8 +33,21 @@ app.get('/', (req, res) => {
 });
 
 
+const createAndSaveUser = (user, done) => {
+  new User({ username: user })
+    .save((err, doc) => {
+      if (err) return done(err);
+      done(null, { _id: doc._id, username: doc.username });
+    });
+}
 
 
+app.post('/api/users', function (req, res) {
+  createAndSaveUser(req.body.username, (err, doc) => {
+    if (err) return res.json(err);
+    return res.json(doc);
+  });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
